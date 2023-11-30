0d46e0601df13298edbd973e9e83d807a065f537
MuhammadFarhanAlAnzhari <100020637+MuhammadFarhanAlAnzhari@users.noreply.github.com>
2023-11-30 22:29:21 +0700

    Create UI Feature Detail About Class and Class Material

---
+++ app/src/main/java/com/group2/sinow/presentation/detail/AboutClassFragment.kt
@@ -0,0 +1,19 @@
+package com.group2.sinow.presentation.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.group2.sinow.R
+
+class AboutClassFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_class, container, false)
+    }
+}
---
+++ app/src/main/java/com/group2/sinow/presentation/detail/ClassMaterialFragment.kt
@@ -0,0 +1,18 @@
+package com.group2.sinow.presentation.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.group2.sinow.R
+class ClassMaterialFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_class_material, container, false)
+    }
+
+}
