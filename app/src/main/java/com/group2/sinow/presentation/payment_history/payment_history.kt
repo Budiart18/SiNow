1f69cbeb9cddc30750802aff0e2d48c3a0f8cb6b
randika16 <randikaakhdan66@gmail.com>
2023-11-30 14:47:41 +0700

    Create Feature Payment History

---
+++ app/src/main/java/com/group2/sinow/presentation/payment_history/payment_history.kt
@@ -0,0 +1,12 @@
+package com.group2.sinow.presentation.payment_history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.group2.sinow.R
+
+class payment_history : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_history)
+    }
+}
