132e4ed688daa26d88cbf91e6328454603e18b92
randika16 <randikaakhdan66@gmail.com>
2023-12-14 20:36:38 +0700

    feat: delete AccountFeatureActivity.kt and replaced with AccountFragment

--- app/src/main/java/com/group2/sinow/presentation/account/AccountFragment.kt
+++ app/src/main/java/com/group2/sinow/presentation/account/AccountFragment.kt
@@ -1,11 +1,14 @@
 package com.group2.sinow.presentation.account
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.isVisible
 import com.group2.sinow.R
-import com.group2.sinow.databinding.ActivityAccountFeatureBinding
+import com.group2.sinow.databinding.FragmentAccountBinding
 import com.group2.sinow.presentation.auth.login.LoginActivity
 import com.group2.sinow.presentation.auth.register.RegisterActivity
 import com.group2.sinow.presentation.change_password.ChangePasswordActivity
@@ -14,24 +17,31 @@ import com.group2.sinow.presentation.profile.ProfileActivity
 import com.group2.sinow.presentation.profile.ProfileViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class AccountFeatureActivity : AppCompatActivity() {
+class AccountFragment : Fragment() {
 
-    private val binding : ActivityAccountFeatureBinding by lazy {
-        ActivityAccountFeatureBinding.inflate(layoutInflater)
-    }
+    private var _binding: FragmentAccountBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: ProfileViewModel by viewModel()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setClickListener()
         getData()
         observeData()
     }
 
     private fun observeData() {
-        viewModel.userData.observe(this){
+        viewModel.userData.observe(viewLifecycleOwner){
             if (it.payload != null) {
                 binding.cvAccount.isVisible = true
                 binding.layoutNonloginAccount.root.isVisible = false
@@ -48,25 +58,27 @@ class AccountFeatureActivity : AppCompatActivity() {
 
     private fun setClickListener() {
         binding.tvProfileTitle.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
+            startActivity(Intent(requireContext(), ProfileActivity::class.java))
         }
         binding.tvChangePasswordTitle.setOnClickListener {
-            startActivity(Intent(this, ChangePasswordActivity::class.java))
+            startActivity(Intent(requireContext(), ChangePasswordActivity::class.java))
         }
         binding.tvPaymentHistoryTitle.setOnClickListener {
-            startActivity(Intent(this, PaymentHistoryActivity::class.java))
+            startActivity(Intent(requireContext(), PaymentHistoryActivity::class.java))
         }
         binding.tvLogoutTitle.setOnClickListener {
 
         }
         binding.layoutNonloginAccount.btnLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
         }
         binding.layoutNonloginAccount.btnRegister.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
+            startActivity(Intent(requireContext(), RegisterActivity::class.java))
         }
-        binding.ivBack.setOnClickListener {
-            onBackPressed()
-        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
